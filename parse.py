401e6530a66549cd0fd8fda892f792f34616313c
vedantsinggh <vedantsinggh@gmail.com>
2026-02-17 02:59:22 +0530

    added question search and vim view

--- parse.py
+++ parse.py
@@ -1,20 +1,25 @@
 import requests
+import subprocess
+import sys
 
-url = "https://alfa-leetcode-api.onrender.com/select/raw?titleSlug=two-sum"
+question_name = sys.argv[1]
+url = "https://alfa-leetcode-api.onrender.com/select/raw?titleSlug=" + question_name
 
 try:
     response = requests.get(url)
     response.raise_for_status() 
     data = response.json()
 
-    default_includes = "#include <iostream>\n#include <vector>\n#include <algorithms>\n"
+    default_includes = "#include <iostream>\n#include <vector>\n#include <algorithm>\nusing namespace std;\n"
     question = data["question"]["content"]
     code = data["question"]["codeSnippets"][0]["code"]
 
-    with open("two-sum.cpp", "w") as file:
+    with open(question_name + ".cpp", "w") as file:
         file.write(default_includes)
         file.write("\n")
         file.write(code)
+
+    subprocess.run(["vim", question_name + ".cpp"])
 
 except requests.exceptions.RequestException as e:
     print(f"An error occurred: {e}")
