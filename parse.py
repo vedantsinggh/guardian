cfefaed4720e78c9dbc6b435b0e4b02de9163bf0
vedantsinggh <vedantsinggh@gmail.com>
2026-02-18 00:54:07 +0530

    added expectional handling and todos

--- parse.py
+++ parse.py
@@ -1,3 +1,8 @@
+#TODO: add testcase
+#TODO: add better question and notes taking ability
+#TODO: make standalone installable application
+
+
 import requests
 import subprocess
 import sys
@@ -11,15 +16,19 @@ try:
     response.raise_for_status() 
     data = response.json()
 
+    if data["question"] is None or data is None:
+        print("Cant find the specified question!")
+        exit(-1)
+
     default_includes = "#include <iostream>\n#include <vector>\n#include <algorithm>\nusing namespace std;\n"
     question = data["question"]["content"]
     #question.replace("\n", " \n ")
+
     code = data["question"]["codeSnippets"][0]["code"]
     topic = data["question"]["topicTags"][0]["slug"]
 
     if not os.path.exists(topic):
         os.mkdir(topic)
-
 
     if not os.path.exists(topic + "/" + question_name):
         os.mkdir(topic + "/" + question_name)
